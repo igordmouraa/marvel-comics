1a3e7c33d97056f8ff19b94c07d2d2ee51a2540a
igordmouraa <igordmoura_@hotmail.com>
2025-02-25 17:07:44 -0300

    feat: update layout to include Footer, enhance metadata, and apply Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,32 @@
 import "../styles/globals.css";
 import { ReactNode } from "react";
 import Navbar from "../components/Navbar";
-import Head from "next/head";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const metadata = {
+    title: "Marvel Universe | Explore Comics e Personagens",
+    description: "Descubra o universo Marvel através de quadrinhos, personagens e eventos épicos. Sua jornagem heróica começa aqui!",
+    openGraph: {
+        images: "/og-image.jpg",
+    },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="pt-BR">
-      <Head>
-        <title>Marvel Comics</title>
-      </Head>
-      <body className="bg-gray-100 text-gray-900">
+    return (
+        <html lang="pt-BR" className="scroll-smooth">
+        <body className={`${inter.variable} font-sans bg-white text-gray-950 antialiased`}>
         <Navbar />
-        <main className="container mx-auto px-4 py-6">{children}</main>
-      </body>
-    </html>
-  );
+
+
+        <main className="min-h-[calc(100vh-160px)]">
+            {children}
+        </main>
+
+        <Footer />
+        </body>
+        </html>
+    );
 }
